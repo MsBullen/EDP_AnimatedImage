17dd0b166438084e18a555ffc99021510247e0ff
HBullen <HBullen@ACER-ITDEPT-001>
2024-05-27 15:21:14 +0100

    Add project files.

---
+++ EDP_AnimatedImage/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EDP_AnimatedImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.refreshTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(270, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Animated Gif Image";
+            // 
+            // refreshTimer
+            // 
+            this.refreshTimer.Enabled = true;
+            this.refreshTimer.Interval = 20;
+            this.refreshTimer.Tick += new System.EventHandler(this.refresh);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 599);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animated Image Example";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPaint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer refreshTimer;
+    }
+}
+
---
+++ EDP_AnimatedImage/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EDP_AnimatedImage
+{
+    public partial class Form1 : Form
+    {
+        private Image panda; //holds an image of the panda
+        private List<string> pandaMovements = new List<string>();//stores the filenames of panda frame images
+        private int steps = 0; //identifies index of pandaMovements list
+        private int slowDownFrameRate = 0; //used to slow down the animation
+
+        public Form1()
+        {
+            InitializeComponent(); 
+            this.DoubleBuffered = true;  //needed for smooth drawing of images
+           // panda = Image.FromFile($"images/wavingPanda.gif");
+
+
+            pandaMovements = Directory.GetFiles($"images/pandaframes", "*.gif").ToList();//load image file names into a list
+            panda = Image.FromFile(pandaMovements[0]);// set the initial player image to the first item in the list
+        }
+
+        private void refresh(object sender, EventArgs e)
+        {//timer event
+            this.Invalidate();
+            animateImage(0, pandaMovements.Count()-1);
+        }
+        
+        private void FormPaint(object sender, PaintEventArgs e)
+        {//Form paint event
+            Graphics Canvas = e.Graphics;
+            Canvas.DrawImage(panda, 174, 65, 480, 480);
+        }
+         
+        private void animateImage(int start, int end)
+        {//procedure to change the image file, incrementing the image location index each time it is called
+
+            slowDownFrameRate += 1;
+            if (slowDownFrameRate == 20) //wait until the procedure has been called a number of times before changing the image
+                                        //this slows down the rate at which the image changes
+            {
+                steps++; //go to next image
+                slowDownFrameRate = 0; //frame rate countdown again
+            }
+            if (steps > end || steps < start) //identify when to loop back to first image
+            {
+                steps = start;
+            }
+            panda = Image.FromFile(pandaMovements[steps]);//change the image
+
+        }
+    }
+}
